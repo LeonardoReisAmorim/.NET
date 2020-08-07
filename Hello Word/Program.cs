4150370b08b78ada17a190753160c67ac8b71cfc
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-08-06 23:43:48 -0300

    import first project

---
+++ Hello Word/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
