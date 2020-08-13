2b5dc2cd4f7c200184f34bb9b8ae89babb51ff48
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-08-13 15:41:38 -0300

    Update Program.cs

--- Hello Word/Program.cs
+++ Hello Word/Program.cs
@@ -6,7 +6,16 @@ namespace Hello_Word
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.WriteLine("digite seu nome");
+            String nome = Console.ReadLine();
+            Console.WriteLine("digite quantos quartos tem");
+            int qt = int.Parse(Console.ReadLine());
+            Console.WriteLine("entre com os precos do produto");
+            double preco = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("seu nome e: "+nome);
+            Console.WriteLine("tem :"+qt+" quartos");
+            Console.WriteLine("o seu produto custa: "+preco);
         }
     }
 }
