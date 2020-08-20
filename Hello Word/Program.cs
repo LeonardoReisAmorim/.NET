4fff0fd7c6ac8868f23db62bcac05fb14dc5b720
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-08-20 15:34:24 -0300

    Update Program.cs

--- Hello Word/Program.cs
+++ Hello Word/Program.cs
@@ -6,22 +6,9 @@ namespace Hello_Word
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("numero da peca");
-            int npeca1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("qt da peca");
-            int qtpeca1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("valor da peca");
-            double vpeca1 = double.Parse(Console.ReadLine());
-            Console.WriteLine("numero da peca");
-            int npeca2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("qt da peca");
-            int qtpeca2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("valor da peca");
-            double vpeca2 = double.Parse(Console.ReadLine());
-
-            double conta = (qtpeca1*vpeca1)+(qtpeca2*vpeca2);
-                            //1*10 = 10         2*20=40          10+40=50
-            Console.WriteLine("o resultado e: "+conta);
+            Console.WriteLine("digite o primeiro numero");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(n1);
         }
     }
 }
