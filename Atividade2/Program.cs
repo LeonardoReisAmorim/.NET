ac612c53bd2c42b3ea40f27047f22b306f310834
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-08-27 17:17:01 -0300

    u

--- Atividade2/Program.cs
+++ Atividade2/Program.cs
@@ -6,8 +6,33 @@ namespace Atividade2
     {
         static void Main(string[] args)
         {
-            
+            /*Console.WriteLine("digite o nome");
+            string nome1 = Console.ReadLine();
+            Console.WriteLine("digite a idade");
+            int idade1 = int.Parse(Console.ReadLine());
+            Pessoa p1 = new Pessoa(nome1,idade1);
+
+            Console.WriteLine("digite o nome");
+            string nome2 = Console.ReadLine();
+            Console.WriteLine("digite a idade");
+            int idade2 = int.Parse(Console.ReadLine());
+            Pessoa p2 = new Pessoa(nome2,idade2);*/
+
             Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+            Console.WriteLine("digite o nome");
+            p1.Nome = Console.ReadLine();
+            p1.Idade = int.Parse(Console.ReadLine());
+            p2.Nome = Console.ReadLine();
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            int maior = p1.maioridade(p1.Idade, p2.Idade);
+
+            if(){
+
+            } else if(maior==p1.Idade){
+                Console.WriteLine(p1.Nome);
+            }
         }
     }
 }
