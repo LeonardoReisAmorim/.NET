4ec72201e42087e36385d79d7e627e60db04d45e
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-08-27 17:00:23 -0300

    Create Pessoa.cs

---
+++ Atividade2/Pessoa.cs
@@ -0,0 +1,21 @@
+namespace Atividade2
+{
+    public class Pessoa
+    {
+        
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa(string nome, int idade)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+        }
+
+        public int maioridade(int idade1, int idade2){
+            if(idade1>idade2){
+
+            }
+        }
+    }
+}
