9b0ff311412663a15977dc8190df90b5ec3e2670
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-08-27 15:45:45 -0300

    Update produto.cs

--- Hello Word/produto.cs
+++ Hello Word/produto.cs
@@ -2,6 +2,33 @@ namespace Hello_Word
 {
     public class produto
     {
-        public int MyProperty { get; set; }
+        
+        public string nome { get; set; }
+        public double preco { get; set; }
+        public int quantidade { get; set; }
+
+        public produto(string nome, double preco, int quantidade)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidade = quantidade;
+
+        }
+
+        public produto(){
+
+        }
+
+        public double valortotalestoque(){
+            return preco*quantidade;
+        }
+
+        public int addprodutos(int qt){
+            return quantidade+qt;
+        }
+
+        public int removeproduto(int qt){
+            return quantidade-qt;
+        }
     }
 }
