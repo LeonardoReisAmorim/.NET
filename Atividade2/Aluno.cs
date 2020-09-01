e3eca2b9ae74b3e0c1f87c16ede0c095672815e9
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-09-01 19:40:48 -0300

    Update Aluno.cs

--- Atividade2/Aluno.cs
+++ Atividade2/Aluno.cs
@@ -2,21 +2,49 @@ namespace Atividade2
 {
     public class Aluno
     {
+
         
         public string Nome { get; set; }
         public int Nota1 { get; set; }
         public int Nota2 { get; set; }
         public int Nota3 { get; set; }
-        public double Media { get; set; }
+        public int Notatotal { get; set; }
+        public int Notaminima { get; set; }
 
-        public Aluno(string nome, int nota1, int nota2, int nota3, double media)
+        public Aluno(string nome, int nota1, int nota2, int nota3)
         {
             this.Nome = nome;
             this.Nota1 = nota1;
             this.Nota2 = nota2;
             this.Nota3 = nota3;
-            this.Media = media;
-
+            this.Notaminima = 60;
         }
+
+        public int verificar_primeiro_semestre()
+        {
+            int soma = Nota1 + Nota2 + Nota3;
+            if (soma >= 30)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public int verificar_segeter_semestre()
+        {
+            int soma = Nota1 + Nota2 + Nota3;
+            if (soma >= 35)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
     }
 }
