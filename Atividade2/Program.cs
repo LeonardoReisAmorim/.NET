12b0dbdba1bad94f4c6521463b5c051ae1c9aff3
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-09-03 18:59:54 -0300

    Update Program.cs

--- Atividade2/Program.cs
+++ Atividade2/Program.cs
@@ -12,7 +12,16 @@ namespace Atividade2
                Console.Write("terceirizado: 1/2 (1 = é terceirizado, 2 = não é terceirizado");
                int terce = int.Parse(Console.ReadLine());
                if(terce==1){
-                   FuncionarioTerceirizados f = new FuncionarioTerceirizados();
+                   FuncionarioTerceirizados[] f = new FuncionarioTerceirizados[i];
+                   Console.Write("digite seu nome: "); 
+                   f[i].Nome = Console.ReadLine();
+                   Console.Write("digite a qt de horas trabalhadas: "); 
+                   f[i].HorasTrabalhadas = int.Parse(Console.ReadLine());
+                   Console.Write("digite o valor por hora: "); 
+                   f[i].ValorPorHora = double.Parse(Console.ReadLine());
+                   Console.Write("digite a despeja adicional: "); 
+                   f[i].DespejaAdicional = double.Parse(Console.ReadLine());
+                   Console.WriteLine("pagamento: "+f[i].pagamento());
                } 
            }
         }
