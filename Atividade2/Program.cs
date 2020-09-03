931474d1339e13c4f8d096c6df492fb3d5b2e0c1
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-09-03 16:24:02 -0300

    Update Program.cs

--- Atividade2/Program.cs
+++ Atividade2/Program.cs
@@ -8,7 +8,15 @@ namespace Atividade2
         {
            Conta c1 = new Conta("Maria", 1010, 200.00);
 
-           
+           Console.WriteLine("Mostrar saldo atual de: "+c1.Nome+" saldo: "+c1.Saldo);
+           Console.Write("Depositar: ");
+           double valordeposito = double.Parse(Console.ReadLine());
+           c1.depositar(valordeposito);
+           Console.WriteLine("Mostrar saldo atual de: "+c1.Nome+" saldo: "+c1.Saldo); 
+           Console.Write("sacar: ");
+           double valorsaque = double.Parse(Console.ReadLine());
+           c1.sacar(valorsaque);
+           Console.WriteLine("Mostrar saldo atual de: "+c1.Nome+" saldo: "+c1.Saldo); 
         }
     }
 }
