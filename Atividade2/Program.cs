249f761eb9a88b9046eb2bdf66a6bb7e78c83a2a
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-09-03 19:46:48 -0300

    u

--- Atividade2/Program.cs
+++ Atividade2/Program.cs
@@ -8,12 +8,14 @@ namespace Atividade2
         {
            Console.WriteLine("digite quantos funcionarios existem:");
            int x = int.Parse(Console.ReadLine());
+           FuncionarioTerceirizados[] ft = new FuncionarioTerceirizados[x];
+           Funcionario[] f= new Funcionario[x];
            for(int i=0;i<x;i++){
-               Console.WriteLine("FUNCIONARIO NUMERO: "+i+1);
-               Console.Write("terceirizado: 1/2 (1 = é terceirizado, 2 = não é terceirizado");
+               Console.WriteLine("FUNCIONARIO NUMERO: "+(i+1));
+               Console.WriteLine("terceirizado: 1/2 (1 = é terceirizado, 2 = não é terceirizado");
                int terce = int.Parse(Console.ReadLine());
                if(terce==1){
-                   FuncionarioTerceirizados[] ft = new FuncionarioTerceirizados[i];
+                   ft[i] = new FuncionarioTerceirizados();
                    Console.Write("digite seu nome: "); 
                    ft[i].Nome = Console.ReadLine();
                    Console.Write("digite a qt de horas trabalhadas: "); 
@@ -23,8 +25,8 @@ namespace Atividade2
                    Console.Write("digite a despeja adicional: "); 
                    ft[i].DespejaAdicional = double.Parse(Console.ReadLine());
                    Console.WriteLine("pagamento: "+ft[i].pagamento());
-               }else if(terce==2){
-                   Funcionario[] f= new Funcionario[i];
+               }else if(terce==2){ 
+                   f[i] = new Funcionario();
                    Console.Write("digite seu nome");
                    f[i].Nome = Console.ReadLine();
                    Console.Write("digite a qt de horas trabalhadas: ");
