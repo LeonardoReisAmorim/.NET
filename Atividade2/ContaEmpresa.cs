8e85e7ae204951d10611e805eb7ebb06e48745be
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-09-03 17:46:02 -0300

    Update ContaEmpresa.cs

--- Atividade2/ContaEmpresa.cs
+++ Atividade2/ContaEmpresa.cs
@@ -2,6 +2,23 @@ namespace Atividade2
 {
     public class ContaEmpresa : Conta
     {
-       
+        public double LimiteEmprestimo { get; set; }
+
+        public ContaEmpresa(double limiteEmprestimo)
+        {
+            this.LimiteEmprestimo = limiteEmprestimo;
+        }
+
+        public ContaEmpresa(){
+
+        }
+
+        public void emprestar(double valor){
+            if(valor>this.LimiteEmprestimo){
+                this.Saldo = this.Saldo;
+            }else{
+                this.Saldo += valor;
+            }
+        }
     }
 }
