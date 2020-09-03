92a6dba8081629c861ca159d83486da0279ef663
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-09-03 18:28:35 -0300

    Update FuncionarioTerceirizados.cs

--- Atividade2/FuncionarioTerceirizados.cs
+++ Atividade2/FuncionarioTerceirizados.cs
@@ -1,7 +1,19 @@
 namespace Atividade2
 {
-    public class FuncionarioTerceirizados
+    public class FuncionarioTerceirizados : Funcionario
     {
+        
+        public double DespejaAdicional { get; set; }
+
+        public FuncionarioTerceirizados(double despejaAdicional,string nome, int horasTrabalhadas, double valorPorHora) : base(nome, horasTrabalhadas, valorPorHora)
+        {
+            this.DespejaAdicional = despejaAdicional;
+        }
+
+        public FuncionarioTerceirizados(){
+
+        }
+
         
     }
 }
