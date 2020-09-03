830534f2a3d06c95b6eaf7108788c1bd6a067f4b
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-09-03 16:30:37 -0300

    Update Program.cs

--- Atividade2/Program.cs
+++ Atividade2/Program.cs
@@ -9,25 +9,25 @@ namespace Atividade2
            Conta c1 = new Conta("Maria", 1010, 200.00);
            Conta c2 = new Conta("pedro", 1234, 100.00);
 
-           Console.WriteLine("Mostrar saldo atual de: "+c1.Nome+" saldo: "+c1.Saldo);
+           Console.WriteLine("Mostrar saldo atual de: "+c1.Nome+" saldo: "+c1.Saldo.ToString("F2"));
            Console.Write("Depositar: ");
            double valordeposito = double.Parse(Console.ReadLine());
            c1.depositar(valordeposito);
-           Console.WriteLine("Mostrar saldo atual de: "+c1.Nome+" saldo: "+c1.Saldo); 
+           Console.WriteLine("Mostrar saldo atual de: "+c1.Nome+" saldo: "+c1.Saldo.ToString("F2")); 
            Console.Write("sacar: ");
            double valorsaque = double.Parse(Console.ReadLine());
            c1.sacar(valorsaque);
-           Console.WriteLine("Mostrar saldo atual de: "+c1.Nome+" saldo: "+c1.Saldo); 
+           Console.WriteLine("Mostrar saldo atual de: "+c1.Nome+" saldo: "+c1.Saldo.ToString("F2")); 
 
-           Console.WriteLine("Mostrar saldo atual de: "+c2.Nome+" saldo: "+c2.Saldo);
+           Console.WriteLine("Mostrar saldo atual de: "+c2.Nome+" saldo: "+c2.Saldo.ToString("F2"));
            Console.Write("Depositar: ");
            double valordeposito2 = double.Parse(Console.ReadLine());
            c1.depositar(valordeposito2);
-           Console.WriteLine("Mostrar saldo atual de: "+c2.Nome+" saldo: "+c2.Saldo); 
+           Console.WriteLine("Mostrar saldo atual de: "+c2.Nome+" saldo: "+c2.Saldo.ToString("F2")); 
            Console.Write("sacar: ");
            double valorsaque2 = double.Parse(Console.ReadLine());
            c1.sacar(valorsaque2);
-           Console.WriteLine("Mostrar saldo atual de: "+c2.Nome+" saldo: "+c2.Saldo);
+           Console.WriteLine("Mostrar saldo atual de: "+c2.Nome+" saldo: "+c2.Saldo.ToString("F2"));
         }
     }
 }
