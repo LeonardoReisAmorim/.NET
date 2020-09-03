52ea24efaa1a27d86c24ef9f76574c23355a0017
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-09-03 20:31:02 -0300

    Update ProdutoUsado.cs

--- Atividade2/ProdutoUsado.cs
+++ Atividade2/ProdutoUsado.cs
@@ -1,7 +1,17 @@
 namespace Atividade2
 {
-    public class ProdutoUsado
+    public class ProdutoUsado : Produto
     {
         
+        public int AnoFabricacao { get; set; }
+
+        public ProdutoUsado(int anoFabricacao,string nome, double preco) : base(nome, preco)
+        {
+            this.AnoFabricacao = anoFabricacao;
+        }
+
+        public override string Etiqueta(){
+            return "Produto: "+this.Nome+"(usado) preço: "+this.Preco+" ano de fabricação: "+this.AnoFabricacao;
+        }
     }
 }
