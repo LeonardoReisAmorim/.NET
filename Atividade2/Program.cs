fd22919c38065a9e3932438fd4fec871b1785528
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-09-03 19:09:32 -0300

    u

--- Atividade2/Program.cs
+++ Atividade2/Program.cs
@@ -9,6 +9,7 @@ namespace Atividade2
            Console.WriteLine("digite quantos funcionarios existem:");
            int x = int.Parse(Console.ReadLine());
            for(int i=0;i<x;i++){
+               Console.WriteLine("FUNCIONARIO NUMERO: "+i+1);
                Console.Write("terceirizado: 1/2 (1 = é terceirizado, 2 = não é terceirizado");
                int terce = int.Parse(Console.ReadLine());
                if(terce==1){
@@ -31,6 +32,8 @@ namespace Atividade2
                    Console.Write("digite o valor por hora: "); 
                    f[i].ValorPorHora = double.Parse(Console.ReadLine());
                    Console.WriteLine("pagamento: "+f[i].pagamento());
+               }else{
+                   Console.WriteLine("digite um numero valido");
                } 
            }
         }
