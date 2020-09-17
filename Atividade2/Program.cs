83e42b008bf73fb9ee20d80c75e84ccefc2ab460
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-09-17 12:53:43 -0300

    u

--- Atividade2/Program.cs
+++ Atividade2/Program.cs
@@ -9,17 +9,27 @@ namespace Atividade2
            List<Pessoa> list = new List<Pessoa>();
            Pessoa p1 = new Pessoa("leo", 30);
            Pessoa p2 = new Pessoa("maria", 17);
+           Pessoa p3 = new Pessoa("joao", 25);
+           Pessoa p4 = new Pessoa("alice", 16);
 
            list.Add(p1);
            list.Add(p2);
+           list.Add(p3);
+           list.Add(p4);
 
            //foreach(Pessoa i in list){
             //   Console.WriteLine(i.Idade);
            //} 
 
-           Pessoa res = list.Find(x => x.Nome.Equals("leo"));
+           //Pessoa res = list.Find(x => x.Nome.Equals("leo"));
           
-               Console.WriteLine(res.Idade+" "+res.Nome);
+            //   Console.WriteLine(res.Idade+" "+res.Nome);
+
+            List<Pessoa> novalista = list.FindAll(x => x.Idade <= 18);
+
+            foreach(Pessoa i in novalista){
+                Console.WriteLine(i.Nome+" "+i.Idade);
+            }
           
 
            
