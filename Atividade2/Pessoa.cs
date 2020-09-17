164e8c13f132abf9de6eaa2b5fbfcab0611b4b47
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-09-17 12:16:27 -0300

    Create Pessoa.cs

---
+++ Atividade2/Pessoa.cs
@@ -0,0 +1,16 @@
+namespace Atividade2
+{
+    public class Pessoa
+    {
+        
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa(string nome, int idade) 
+        {
+            this.Nome = nome;
+                this.Idade = idade;
+               
+        }
+    }
+}
