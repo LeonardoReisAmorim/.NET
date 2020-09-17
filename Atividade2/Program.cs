eb52e10bda2fed08d6e7b477ac548f55fa039d97
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-09-17 12:13:42 -0300

    Update Program.cs

--- Atividade2/Program.cs
+++ Atividade2/Program.cs
@@ -6,60 +6,6 @@ namespace Atividade2
     {
         static void Main(string[] args)
         {
-            /*
-           Console.WriteLine("digite quantos funcionarios existem:");
-           int x = int.Parse(Console.ReadLine());
-           FuncionarioTerceirizados[] ft = new FuncionarioTerceirizados[x];
-           Funcionario[] f= new Funcionario[x];
-           for(int i=0;i<x;i++){
-               Console.WriteLine("FUNCIONARIO NUMERO: "+(i+1));
-               Console.WriteLine("terceirizado: 1/2 (1 = é terceirizado, 2 = não é terceirizado");
-               int terce = int.Parse(Console.ReadLine());
-               if(terce==1){
-                   ft[i] = new FuncionarioTerceirizados();
-                   Console.Write("digite seu nome: "); 
-                   ft[i].Nome = Console.ReadLine();
-                   Console.Write("digite a qt de horas trabalhadas: "); 
-                   ft[i].HorasTrabalhadas = int.Parse(Console.ReadLine());
-                   Console.Write("digite o valor por hora: "); 
-                   ft[i].ValorPorHora = double.Parse(Console.ReadLine());
-                   Console.Write("digite a despeja adicional: "); 
-                   ft[i].DespejaAdicional = double.Parse(Console.ReadLine());
-                   Console.WriteLine("pagamento: "+ft[i].pagamento());
-               }else if(terce==2){ 
-                   f[i] = new Funcionario();
-                   Console.Write("digite seu nome: ");
-                   f[i].Nome = Console.ReadLine();
-                   Console.Write("digite a qt de horas trabalhadas: ");
-                   f[i].HorasTrabalhadas = int.Parse(Console.ReadLine()); 
-                   Console.Write("digite o valor por hora: "); 
-                   f[i].ValorPorHora = double.Parse(Console.ReadLine());
-                   Console.WriteLine("pagamento: "+f[i].pagamento());
-               }else{
-                   Console.WriteLine("digite um numero valido");
-               } 
-           }
-           */
-
-           List<string> list = new List<string>();
-
-           list.Add("leonardo");
-           list.Add("maria");
-           list.Add("carlos");
-           list.Add("caio");
-
-           //foreach(string i in list){
-           //    Console.WriteLine(i);
-           //}
-
-           //string res = list.Find(x => x=="leonardo");
-           //Console.WriteLine(res);
-
-           List<string> newlist = list.FindAll(x => x.Length>5);
-
-           foreach(string i in newlist){
-               Console.WriteLine(i);
-           }
             
         }
     }
