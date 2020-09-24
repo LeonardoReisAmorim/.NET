d65edf311a43ac254027d6ce5c820b7f8c7ec875
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-09-24 17:08:28 -0300

    Update Program.cs

--- exercicios/Program.cs
+++ exercicios/Program.cs
@@ -12,7 +12,10 @@ namespace exercicios
            Produto p2 = new Produto("c", "d", 20.0);
            Produto p3 = new Produto("e", "f", 30.0);
 
-           
+           Compra c1 = new Compra("leo", 001);
+           c1.Produtos.Add(p1);
+           c1.Produtos.Add(p2);
+           c1.Produtos.Add(p3);
         }
     }
 }
